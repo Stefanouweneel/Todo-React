28529ca973b8d7cc81902a874d813810b37615d3
Stefan Ouweneel <stefanouweneel@gmail.com>
2016-07-07 09:29:02 +0200

    Add todo model spec

---
+++ spec/models/todo_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe Todo do
+
+  describe "validations" do
+
+    it "is invalid without a title" do
+      todo = Todo.new(title: "")
+      todo.valid?
+      expect(todo.errors).to have_key(:title)
+    end
+    
+  end
+
+end
