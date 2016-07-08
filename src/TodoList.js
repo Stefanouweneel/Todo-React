612a1e965994695662eb9c9949b4051e7267c3c2
Stefan Ouweneel <stefanouweneel@gmail.com>
2016-07-08 14:42:13 +0200

    Commit per Ben's request

--- src/TodoList.js
+++ src/TodoList.js
@@ -9,40 +9,63 @@ class TodoList extends React.Component {
     super();
 
     this.state = {
-    todos: [],
-    counts:
-      { todo: 0, done: 0},
+      message: "There are no tasks yet.",
+      todos: []
     };
-
   }
 
-  $.ajax({
-    type: "PUT",
-    url: "/todos/" + todoId + ".json",
-    data: JSON.stringify({
-      todo: { completed: isCompleted }
-    }),
-    contentType: "application/json",
-    dataType: "json"
-  })
-
-  createTodo() {
-    return (
-
-    );
+  componentDidMount(){
+    var self = this;
+    $.getJSON("https://secret-everglades-41596.herokuapp.com", function(data){
+      self.setState({
+        todos: data.todos
+      });
+    });
   }
 
-  clearTodos() {
-    return (
+  renderTodo(todo){
+    return <Todo
+      title={todo.title}
+      completed={todo.completed} />;
+  } 
 
-    );
-  }
+  renderTodos(todo){
+     $.ajax({
+       type: "POST",
+       url: "https://secret-everglades-41596.herokuapp.com/todos.json",
+       data: JSON.stringify({
+           todos: todo
+       }),
+       contentType: "application/json",
+       dataType: "json"
+     });
+   }
+
+  // componentDidMount() {
+  //   this.loadTodos();
+  // }
+  //
+  // loadTodos() {
+  //   let component = this;
+  // }
+  //
+  // createTodo() {
+  //   return (
+  //
+  //   );
+  // }
+  //
+  // clearTodos() {
+  //   return (
+  //
+  //   );
+  // }
 
   render() {
     return (
       <div>
-        	<form onSubmit={this.createTodo} />
-          <form onSubmit={this.clearTodos} />
+        <h1>Whatever</h1>
+          <p> {this.state.todos.map(this.renderTodo.bind(this))}</p>
       </div>
     );
   }
